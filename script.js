6363a059e553eb685f17b0f371006edaa45d7759
acmcgowan2”git config --global user.email “acmcgowan2@gmail.com” <student@Cumberlandss-MacBook-Pro-5.local>
2022-03-03 13:30:12 -0600

    added LiteralMathMethodExercise

---
+++ script.js
@@ -0,0 +1,38 @@
+let warmHugs = "Hi, I'm Olaf and I like warm hugs";
+
+console.log(warmHugs.toUpperCase());
+
+warmHugs = warmHugs.replace("like", "love");
+
+let beenImpaled = "Oh, look at that. I've been impaled.";
+
+beenImpaled = beenImpaled.slice(-18);
+
+let dotDotDot = "..."
+
+let skullBones = `I dont't have a skull ${dotDotDot} or bones.`;
+
+console.log(Math.PI);
+
+let randomNumber = Math.random();
+randomNumber *=3;
+randomNumber = Math.floor(randomNumber);
+randomNumber++;
+
+// BONUS!!!
+console.log(" Let It Go!".toUpperCase().repeat(2).trim());
+
+let reindeers = "Reindeers are better than people.";
+
+console.log(reindeers.replace("Reindeers are better than people.","Reindeers_are_better_than_people."));
+
+console.log(Math.SQRT2);
+
+let newRandomNumber = Math.random();
+newRandomNumber *=17;
+newRandomNumber = Math.floor(newRandomNumber);
+newRandomNumber += 7;
+console.log(newRandomNumber);
+
+
+
